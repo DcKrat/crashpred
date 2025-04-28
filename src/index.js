8e19b52caf27c6e52a7638b3d53ba875867cc8ae
DcKrat <bogdanovkirill123@gmail.com>
2025-04-28 11:40:57 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CrashPredictor from "./CrashPredictor";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <CrashPredictor />
+  </React.StrictMode>
+);
